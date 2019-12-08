e8c7d0405f4a883d0d80f23adcc2f3e0d9c252da
Ruqaiya Arif <35794356+Ruqaiya53@users.noreply.github.com>
2019-12-07 19:55:38 -0500

    Create Arithmetic_operators.py

---
+++ Python/Arithmetic_operators.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    sumOfab=a+b
+    diffOfab=a-b
+    multOfab=a*b
+    print("{}\n{}\n{}".format(sumOfab,diffOfab,multOfab))
